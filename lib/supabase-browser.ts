2a337abd9cda0c6eb7a8b258dfcd1aa7e26198a1
asalpeter <101529065+asalpeter@users.noreply.github.com>
2025-10-28 22:55:55 -0400

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,10 +1,16 @@
-'use client';
-
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowserClient } from "@supabase/ssr";
 
 export function getBrowserSupabase() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: "implicit",
+        detectSessionInUrl: true,
+        persistSession: true,
+        autoRefreshToken: true,
+      },
+    }
   );
 }
