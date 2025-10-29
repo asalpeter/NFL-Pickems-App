d3a9ad4769c9a388b31d1c806c3150f96ba32b9f
asalpeter <101529065+asalpeter@users.noreply.github.com>
2025-10-29 01:48:55 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             <Link href="/" className="text-xl font-bold">🏈 Pick'ems</Link>
             <nav className="flex gap-4 text-sm items-center">
               <Link href="/dashboard" className="link">Dashboard</Link>
-              {/* @ts-expect-error Async Server Component */}
               <AuthMenu />
               <a href="https://github.com/" target="_blank" className="link">GitHub</a>
             </nav>
